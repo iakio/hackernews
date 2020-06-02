16bca6a131c140a52c1d56cd589ca543adb49305
ISHIDA Akio <iakio.monospacenet@gmail.com>
2020-06-03 00:55:33 +0900

    [client]connect to the server

--- assets/js/components/LinkList.js
+++ assets/js/components/LinkList.js
@@ -1,28 +1,34 @@
 import React from "react"
 import Link from "./Link"
+import gql from "graphql-tag"
+import { Query } from "react-apollo"
 
-const linksToRender = [
+const FEED_QUERY = gql`
   {
-    id: 1,
-    description: "Prisma turns your database into a GraphQL API 😎",
-    url: "https://www.prismagraphql.com"
-  },
-  {
-    id: 2,
-    description: "The best GraphQL client for React",
-    url: "https://www.apollographql.com/docs/react/"
+    feed {
+      links {
+        id
+        url
+        description
+      }
+    }
   }
-]
+`
 
 export default function LinkList() {
   return (
-    <div>
-      {
-        linksToRender.map(link =>
+    <Query query={FEED_QUERY}>
+      {({ loading, error, data }) => {
+        if (loading) return <div>Fetching</div>
+        if (error) return <div>Error</div>
+
+        const linksToRender = data.feed.links
+
+        return linksToRender.map(link =>
           <Link key={link.id} link={link} />
         )
-      }
-    </div>
+      }}
+    </Query>
   )
 }
 
