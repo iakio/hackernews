13c984a35e76ffb064990cd4126b5f0654b275c0
ISHIDA Akio <iakio.monospacenet@gmail.com>
2020-06-05 10:03:01 +0900

    [server]add signin and signup mutations to schema

--- lib/hackernews_web/schema.ex
+++ lib/hackernews_web/schema.ex
@@ -7,6 +7,20 @@ defmodule HackernewsWeb.Schema do
     field :id, non_null(:id)
     field :url, non_null(:string)
     field :description, non_null(:string)
+    field :posted_by, :user
+  end
+
+  object :auth_payload do
+    field :token, :string
+    field :user, :user
+  end
+
+  object :user do
+    field :id, non_null(:id)
+    field :name, non_null(:string)
+    field :email, non_null(:string)
+    field :password, non_null(:string)
+    field :links, non_null(list_of(non_null(:link)))
   end
 
   object :feed do
@@ -24,6 +38,17 @@ defmodule HackernewsWeb.Schema do
       arg :url, non_null(:string)
       arg :description, non_null(:string)
       resolve &NewsResolver.create_link/3
+    end
+
+    field :signup, type: :auth_payload do
+      arg :name, non_null(:string)
+      arg :email, non_null(:string)
+      arg :password, non_null(:string)
+    end
+
+    field :signin, type: :auth_payload do
+      arg :email, non_null(:string)
+      arg :password, non_null(:string)
     end
   end
 end
