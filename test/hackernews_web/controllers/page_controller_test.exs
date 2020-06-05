3c4e93820c33fbb0419c74c0cab6435fc63a4253
ISHIDA Akio <iakio.monospacenet@gmail.com>
2020-06-05 10:10:29 +0900

    [server]make tests green

--- test/hackernews_web/controllers/page_controller_test.exs
+++ test/hackernews_web/controllers/page_controller_test.exs
@@ -3,6 +3,6 @@ defmodule HackernewsWeb.PageControllerTest do
 
   test "GET /", %{conn: conn} do
     conn = get(conn, "/")
-    assert html_response(conn, 200) =~ "Welcome to Phoenix!"
+    assert html_response(conn, 200) =~ "Hackernews"
   end
 end
