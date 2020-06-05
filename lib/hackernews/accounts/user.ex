2ae8a4d7d0e88cd6e7c5bc5df24f620baba4f292
ISHIDA Akio <iakio.monospacenet@gmail.com>
2020-06-05 10:05:26 +0900

    [server]add gurdian

--- lib/hackernews/accounts/user.ex
+++ lib/hackernews/accounts/user.ex
@@ -1,6 +1,7 @@
 defmodule Hackernews.Accounts.User do
   use Ecto.Schema
   import Ecto.Changeset
+  alias Argon2
 
   schema "users" do
     field :email, :string
@@ -15,5 +16,13 @@ defmodule Hackernews.Accounts.User do
     user
     |> cast(attrs, [:name, :email, :password])
     |> validate_required([:name, :email, :password])
+    |> put_password_hash()
   end
+
+  defp put_password_hash(%Ecto.Changeset{valid?: true, changes: %{password: password}} = changeset) do
+    change(changeset, password: Argon2.hash_pwd_salt(password))
+  end
+
+  defp put_password_hash(changeset), do: changeset
+
 end
