5a82cd43c5d00dc4785ec0ba7ec5ff3ebbcb1f02
ISHIDA Akio <iakio.monospacenet@gmail.com>
2020-06-07 17:21:24 +0900

    [server]signup, signin mutations

--- lib/hackernews_web/news_resolver.ex
+++ lib/hackernews_web/news_resolver.ex
@@ -1,5 +1,7 @@
 defmodule HackernewsWeb.NewsResolver do
   alias Hackernews.News
+  alias Hackernews.Accounts
+  alias Hackernews.Accounts.Guardian
   def feed(_root, _args, _info) do
     links = News.list_links()
     {:ok, %{links: links}}
@@ -7,5 +9,20 @@ defmodule HackernewsWeb.NewsResolver do
 
   def create_link(_root, args, _info) do
     News.create_link(args)
+  end
+
+  def signup(_root, args, _info) do
+    with {:ok, user} <- Accounts.create_user(args),
+      {:ok, token, _claim} <- Guardian.encode_and_sign(user) do
+      {:ok, %{token: token, user: user}}
+    end
+  end
+
+  def signin(_root, %{email: email, password: password}, _info) do
+    user = Accounts.get_user_by_email!(email)
+    if Argon2.verify_pass(password, user.password) do
+      {:ok, token, _claim} = Guardian.encode_and_sign(user)
+      {:ok, %{token: token, user: user}}
+    end
   end
 end
---
+++ test/hackernews_web/schema_test.exs
@@ -0,0 +1,58 @@
+defmodule HackernewsWeb.SchemaTest do
+  use HackernewsWeb.ConnCase
+  alias Hackernews.Accounts
+
+  test "signup", %{conn: conn} do
+    query = """
+      mutation SignUp($name: String!, $email: String!, $password: String!) {
+        signup(name: $name, email: $email, password: $password) {
+          user {
+            id
+          }
+          token
+        }
+      }
+    """
+    conn = post(conn, "/api", %{
+      "query" => query,
+      "variables" => %{
+        "name" => "user name",
+        "email" => "user emal",
+        "password" => "user password",
+      }
+    })
+
+    assert %{"data" => %{"signup" => %{"token" => token, "user" => user}}} = json_response(conn, 200)
+
+    {:ok, claim} = Hackernews.Accounts.Guardian.decode_and_verify(token)
+    assert claim["sub"] == user["id"]
+  end
+
+  test "signin", %{conn: conn} do
+    {:ok, user} = Accounts.create_user(%{
+      "name" => "user name",
+      "email" => "user emal",
+      "password" => "user password",
+    })
+
+    query = """
+      mutation SignIn($email: String!, $password: String!) {
+        signin(email: $email, password: $password) {
+          token
+        }
+      }
+    """
+    conn = post(conn, "/api", %{
+      "query" => query,
+      "variables" => %{
+        "email" => "user emal",
+        "password" => "user password",
+      }
+    })
+    assert %{"data" => %{"signin" => %{"token" => token}}} = json_response(conn, 200)
+
+    {:ok, claim} = Hackernews.Accounts.Guardian.decode_and_verify(token)
+    assert claim["sub"] == Integer.to_string(user.id)
+
+  end
+end
