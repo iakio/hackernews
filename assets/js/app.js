d5c6bf32c44f1c4d8f71b35938e53ef2c7268822
ISHIDA Akio <iakio.monospacenet@gmail.com>
2020-06-09 22:19:35 +0900

    [client]link context

--- assets/js/app.js
+++ assets/js/app.js
@@ -21,15 +21,27 @@ import App from "./components/App"
 import { ApolloProvider } from "react-apollo"
 import { ApolloClient } from "apollo-client"
 import { createHttpLink } from "apollo-link-http"
+import { setContext } from "apollo-link-context"
 import { InMemoryCache } from "apollo-cache-inmemory"
 import { BrowserRouter } from "react-router-dom"
+import { AUTH_TOKEN } from "./constants"
 
 const httpLink = createHttpLink({
   uri: "http://localhost:4000/api"
 })
 
+const authLink = setContext((_, {headers}) => {
+  const token = localStorage.getItem(AUTH_TOKEN)
+  return {
+    headers: {
+      ...headers,
+      authorization: token ? `Bearer ${token}` : ""
+    }
+  }
+})
+
 const client = new ApolloClient({
-  link: httpLink,
+  link: authLink.concat(httpLink),
   cache: new InMemoryCache()
 })
 
